b40624d5dc43580f627a3af6b80028d1ba2f4784
TejaSaii <tejas7833@gmail.com>
2024-01-09 01:26:43 +0530

    Changed environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  // production: true,
-  // apiUrl: "https://flixnet-backend-2e1fc4e0f2cc.herokuapp.com"
+  production: true,
+  apiUrl: "https://flixnet-backend-2e1fc4e0f2cc.herokuapp.com"
   
-  apiUrl: "http://localhost:3000"
+  // apiUrl: "http://localhost:3000"
 }
